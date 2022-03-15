6c62662897a1c76331f79450f43fad712d1d6e33
faith-ware <faithade69@gmail.com>
2022-03-15 09:59:08 -0500

    Added the email send feature

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -163,3 +163,13 @@ REST_FRAMEWORK = {
 #         'user': ['rest_framework.permissions.IsAdminUser']
 #     },
 # }
+
+
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
--- backend/form/views.py
+++ backend/form/views.py
@@ -3,7 +3,34 @@ from rest_framework.authentication import SessionAuthentication, BasicAuthentica
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+import time
+from django.conf import settings
+from django.core.mail import send_mail, EmailMessage
+import threading
+from threading import Thread
+from smtplib import SMTPException
 # Create your views here.
+
+
+class EmailThread(threading.Thread):
+    def __init__(self, subject, message, sender , recipient):
+        self.subject = subject
+        self.message = message
+        self.recipient = recipient
+        self.sender = sender
+        threading.Thread.__init__(self)
+
+    def run (self):
+        try:
+            send_mail(self.subject, self.message, self.sender, [self.recipient])
+            return True
+        except SMTPException as e:        
+            print('There was an error sending an email.')
+            return False
+
+
+def send_html_mail(subject, message, sender , recipient):
+    EmailThread(subject, message, sender , recipient).start()
 
 class CollectForm(APIView):
     authentication_classes = [BasicAuthentication, TokenAuthentication]
@@ -13,6 +40,13 @@ class CollectForm(APIView):
         return Response({"Hello" : "There"})
 
     def post(self, request, format=None):
-        print(request.data)
-        print(request.user.email)
-        return Response({"Yo" : "Recieved"})
+        subject = "Frist email test"
+        message = "This is the message bro"
+        recipient = 'faithade69@gmail.com'
+        sent = send_html_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient)
+        if sent:
+            print(request.data)
+            return Response({"Yo" : "Successful"})
+        elif sent == False:
+            return Response({"Not" : "Done"})
+        return Response({"Status" : "Processing"})
